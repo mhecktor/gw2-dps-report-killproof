89812d2df33289b49b88d743bcd444dd00475e1e
Marco Hecktor <31827345+mhecktor@users.noreply.github.com>
2021-12-09 19:07:55 +0100

    initial upload of files

---
+++ bg_page.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener(
+    function(url, sender, onSuccess) {
+        fetch(url)
+            .then(response => response.json())
+            .then(responseText => onSuccess(responseText))
+        
+        return true;  // Will respond asynchronously.
+    }
+);
